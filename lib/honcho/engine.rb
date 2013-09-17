af7976dd22bb7cd58f7d2c3c376f801aae36a33f
Sachin Singh <singhsachin87@yahoo.com>
2013-09-17 15:22:10 +0530

    methods to read Honcho configuration

--- lib/honcho/engine.rb
+++ lib/honcho/engine.rb
@@ -1,11 +1,41 @@
 module Honcho
+
+  require 'debugger'
+
+  mattr_accessor :configuration
+
   class Engine < ::Rails::Engine
     isolate_namespace Honcho
   end
 
-  def self.configure(&block)
-    unless block_given?
-      raise 'No Block Found.'
+  def self.admin_models(val)
+    self.configuration[:admin_models] = val
+  end
+
+  def self.default_theme(val)
+    self.configuration[:default_theme] = val
+  end
+
+  def self.config
+    self.configuration ||= {}
+    if block_given?
+      yield(self)
+    else
+      raise 'No configuration block found.'
     end
   end
+
 end
+
+
+
+
+
+=begin
+
+Honcho.configure do |config|
+
+  config.resources [:posts, :comments]
+end
+
+=end
---
+++ test/dummy/config/initializers/honcho.rb
@@ -0,0 +1,18 @@
+# Honcho config file. Generated on September 17, 2013 14:47
+# See github.com/hitendrasingh/honcho for more informations
+
+Honcho.config do |config|
+
+
+  ########## Honcho Configuration #############
+
+  
+  # you can set the default theme of your application over here
+  
+  config.default_theme :blue
+
+  # Set the resources which you need to be under admin panel
+
+  config.admin_models  [:post]
+
+end
