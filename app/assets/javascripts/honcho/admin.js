b8970f85233c4887d85381793425d8d2511ca91d
Sachin87 <singhsachin87@yahoo.com>
2014-05-26 19:40:34 +0530

    check/uncheck javascript added for index page

--- app/assets/javascripts/honcho/admin.js
+++ app/assets/javascripts/honcho/admin.js
@@ -1,2 +1,17 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+
+$(function() {
+    $('#selectAll').click(function() {
+        if (this.checked) {
+            $(':checkbox').each(function() {
+                this.checked = true;
+            });
+        } else {
+            $(':checkbox').each(function() {
+                this.checked = false;
+            });
+        }
+    });
+})
