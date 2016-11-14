dfdc7f565608fdf02d0ba6b9691714b7d9b47f52
kapilsdv <kapilsdv@gmail.com>
2016-11-14 20:37:54 +0530

    Search bar ui changes :art:

--- app/assets/javascripts/honcho/admin.js
+++ app/assets/javascripts/honcho/admin.js
@@ -18,10 +18,10 @@ $(function() {
 
   $('form#search').on('click', '.remove_fields', function(event) {
     $(this).prev('input[type=hidden]').val('1');
-    $(this).closest('fieldset').hide();
+    $(this).closest('.ui.form').hide();
     event.preventDefault();
   });
-  
+
   $('form#search').on('click', '.add_fields', function(event) {
     var regexp, time;
     time = new Date().getTime();
